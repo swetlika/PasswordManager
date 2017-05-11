f06c28fb3493d24b01bbde775082c5013fd77105
Raymond Miao <rmiao9835@gmail.com>
2017-05-11 15:43:11 -0500

    moved form into extension

--- test_chrome_ext/fill.js
+++ test_chrome_ext/fill.js
@@ -1,10 +1,12 @@
 var url = window.location.href;
 console.log(url);
 
+var master_pw = "master";
+
 $.ajax({
 	url: "http://127.0.0.1:5000/test",
 	type: "GET",
-	data: url,
+	data: {master_pw: master_pw, url: url},
 	dataType: "json",
 	success: function (data) {
 		//alert(data[0]);
@@ -19,7 +21,7 @@ $.ajax({
 		var form = pwd_field.closest("form");
 		form.css( "background-color", "blue" );
 
-		var usr_field = form.find("input[type='email']");
+		var usr_field = form.find("input[type='email'],input[name='email']");
 		//if we can't find via email, use regex to find name similar to user
 		if (!usr_field.length) {
 			usr_field = form.find("input[name*='user'],input[name*='usr']");
